2df2453d09e673493a44c006e9381022a1696195
samwest <mail.sam.west@gmail.com>
2020-05-05 16:17:10 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,113 @@
+# scrollCue.js
+scrollCue.js is a JavaScript plugin that works without jQuery.  
+Show elements by scrolling.
+
+## Demo
+* [Usage](https://prjct-samwest.github.io/scrollCue/)  
+* [Examples](https://prjct-samwest.github.io/scrollCue/examples.html)  
+* [CSS Customize](https://prjct-samwest.github.io/scrollCue/customize.html)
+
+## Usage
+
+### Step.1 - Include CSS and Javascript file
+Add a CSS link to `<head>` tag.
+```html
+<link rel="stylesheet" href="scrollCue.css">
+```
+Add a JavaScript link to just before the `</body>` tag.
+```html
+<script src="scrollCue.min.js"></script>
+```
+### Step.2 - HTML Mark up
+
+#### Basic
+Set the `"data-cue"` attribute in the target element and specify the animation type.
+```html
+<img src="001.jpg" data-cue="fadeIn">...</img>
+```
+
+#### Parent wrap
+You can wrap it in a parent element by setting the `"data-cues"` attribute.
+```html
+<div data-cues="fadeIn">
+    <img src="001.jpg">
+    <img src="002.jpg">
+    <img src="003.jpg">
+    <img src="004.jpg">
+    <img src="005.jpg">
+    <img src="006.jpg">
+</div>
+```
+
+
+#### Grouping wrap
+You can group target elements by setting the `"data-group"` attribute.  
+With grouping, when the first element is triggered, the remaining elements are also triggered sequentially, regardless of the scroll amount.
+```html
+<div data-cues="fadeIn" data-group="images">
+    <img src="001.jpg">
+    <img src="002.jpg">
+    <img src="003.jpg">
+    <img src="004.jpg">
+    <img src="005.jpg">
+    <img src="006.jpg">
+</div>
+```
+
+
+
+#### Animations
+There are 15 different animations available.
+
+* fadeIn
+* slideInLeft
+* slideInRight
+* slideInDown
+* slideInUp
+* zoomIn
+* zoomOut
+* rotateIn
+* bounceIn
+* bounceInLeft
+* bounceInRight
+* bounceInDown
+* bounceInUp
+* flipInX
+* flipInY
+
+You can add / custom animation by editing slideCue.css  
+*  [CSS Customize](https://prjct-samwest.github.io/scrollCue/customize.html)
+
+### Step.3 - Initialize scrollCue.js
+```javascript
+scrollCue.init();
+```
+
+### More examples
+See below for animation confirmation and other markup.  
+* [Examples](https://prjct-samwest.github.io/scrollCue/examples.html)  
+
+
+## Options
+|Option|Type|Default|Description|
+|---|---|---|---|
+|duration|Number|600|Time to show the element.(ms)|
+|interval|Number|-0.7|Time interval for showing side-by-side elements. (Integer or Real number)<br> You can use negative numbers.<br>`Integer` : (ms)<br>`Real number` : Percentage of duration time of the previous element.|
+|percentage|Number|0.75|Percentage of screen height to start showing elements.(Real number from 0 to 1)|
+
+
+## Methods
+|Method|Arguments|Description|
+|---|---|---|
+|init|options|Initialize scrollCue.js|
+|update| |Update scrollCue.js|
+
+
+## Browser support
+scrollCue.js works on modern browser as Chrome, Firefox, Safari, Edge, IE11.
+
+
+## License
+Created by SamWest.  
+Copyright (c) 2020 SamWest.  
+This plugin is released under the MIT License.
---
+++ scrollCue.js
@@ -0,0 +1,375 @@
+/**-----------------------
+
+ scrollCue.js - ver.1.00
+ URL : https://prjct-samwest.github.io/scrollCue/
+
+ created by SamWest.
+ Copyright (c) 2020 SamWest.
+ This plugin is released under the MIT License.
+
+ -----------------------**/
+
+const scrollCue = (function () {
+
+    let $f = {}, $e, $q;
+    let resizeTimer = 0, scrollEnable = true;
+
+    let $op, $defaultOptions = {
+        duration : 600,
+        interval : -0.7,
+        percentage : 0.75
+    };
+
+    $f = {
+        setEvents : function(){
+
+            window.addEventListener('load',$f.runQuery);
+
+            window.addEventListener('scroll',function () {
+
+                if (scrollEnable) {
+                    requestAnimationFrame(function() {
+                        scrollEnable = true;
+                        $f.setQuery();
+                        $f.runQuery();
+                    });
+                    scrollEnable = false;
+                }
+            });
+
+            window.addEventListener('resize',function () {
+
+                if (resizeTimer > 0) { clearTimeout(resizeTimer); }
+                resizeTimer = setTimeout(function () {
+
+                    $f.searchElements();
+                    $f.setQuery();
+                    $f.runQuery();
+
+                }, 200);
+
+            });
+
+        },
+        setOptions : function (tgt,add) {
+
+            let resultOptions = {};
+
+            if(typeof tgt === 'undefined') return;
+
+            Object.keys(tgt).forEach(function (key) {
+
+                if(Object.prototype.toString.call(tgt[key]) === "[object Object]") {
+
+                    resultOptions[key] = $f.setOptions(tgt[key],add[key]);
+
+                }else{
+
+                    resultOptions[key] = tgt[key];
+
+                    if(typeof add !== 'undefined' && typeof add[key] !== 'undefined'){
+
+                        resultOptions[key] = add[key];
+
+                    }
+                }
+
+            });
+
+            return resultOptions;
+        },
+        searchElements : function () {
+
+            let parents, selector;
+
+            $e = [];
+
+            parents = document.querySelectorAll('[data-cues]:not([data-disabled])');
+
+            for(let i=0; i<parents.length; i++){
+
+                let parent = parents[i];
+
+                for(let j=0; j<parent.children.length; j++){
+
+                    let child = parent.children[j];
+
+                    $f.setAttrPtoC(child, 'data-cue', parent, 'data-cues', '');
+                    $f.setAttrPtoC(child, 'data-duration', parent, 'data-duration', false);
+                    $f.setAttrPtoC(child, 'data-interval', parent, 'data-interval', false);
+                    $f.setAttrPtoC(child, 'data-sort', parent, 'data-sort', false);
+                    $f.setAttrPtoC(child, 'data-addClass', parent, 'data-addClass', false);
+                    $f.setAttrPtoC(child, 'data-group', parent, 'data-group', false);
+
+                }
+
+                parent.setAttribute('data-disabled','true');
+
+            }
+
+            selector = document.querySelectorAll('[data-cue]:not([data-show="true"])');
+
+            for (let i=0; i<selector.length; i++){
+
+                let elm = selector[i];
+
+                $e.push({
+
+                    elm      : elm,
+                    cue      : $f.getAttr(elm,'data-cue','fadeIn'),
+                    duration : Number( $f.getAttr(elm,'data-duration',$op.duration) ),
+                    interval : Number( $f.getAttr(elm,'data-interval',$op.interval) ),
+                    order    : $f.getOrderNumber(elm),
+                    sort     : $f.getAttr(elm,'data-sort',null),
+                    addClass : $f.getAttr(elm,'data-addClass',null),
+                    group    : $f.getAttr(elm,'data-group',null)
+
+                });
+
+            }
+
+        },
+        sortElements : function(){
+
+            let obj = arguments[0];
+            let keys = [].slice.call(arguments).slice(1);
+
+            for(let i=0; i<keys.length; i++){
+
+                obj.sort(function (a,b) {
+
+                    let asc = typeof keys[i][1] === 'undefined' ? true : keys[i][1];
+                    let key = keys[i][0];
+
+                    if(a[key] > b[key]) return asc ? 1 : -1;
+                    if(a[key] < b[key]) return asc ? -1 : 1;
+
+                    return 0;
+
+                });
+
+            }
+
+        },
+        randElements : function(array){
+
+            for(let i = array.length - 1; i > 0; i--){
+
+                let j = Math.floor(Math.random() * (i + 1));
+                let tmp = array[i];
+                array[i] = array[j];
+                array[j] = tmp;
+
+            }
+
+            return array;
+
+        },
+        setDurationValue : function(interval, pElm, cInt){
+
+            if(typeof pElm === 'undefined'){
+                return interval;
+            }
+
+            let result = interval;
+            let pDura = pElm.duration;
+
+            if(!((cInt+'').indexOf('.') !== -1)){
+
+                result = result + pDura + cInt;
+
+            }else{
+
+                result = result + pDura + (pDura * cInt);
+
+            }
+
+            result = result < 0 ? 0 : result;
+
+            return result;
+
+        },
+        getOrderNumber : function(elm){
+
+            if(elm.hasAttribute('data-order')){
+
+                let num = Number(elm.getAttribute('data-order'));
+
+                return num >= 0 ? num : Math.pow(2, 53) - 1 + num;
+
+            }else{
+
+                return  Math.pow(2, 52) - 1;
+
+            }
+
+        },
+        setAttrPtoC : function (child ,attrC, parent, attrP, value) {
+
+            if(parent.hasAttribute(attrP)){
+
+                if(!child.hasAttribute(attrC)){
+
+                    child.setAttribute(attrC, parent.getAttribute(attrP));
+
+                }
+
+            }else{
+
+                if(value !== false){
+
+                    child.setAttribute(attrC, value);
+
+                }
+
+            }
+
+        },
+        getAttr : function (elm, attr, value) {
+
+            if(elm.hasAttribute(attr)){
+
+                return elm.getAttribute(attr);
+
+            }else{
+
+                return value;
+
+            }
+
+        },
+        getOffsetTop : function(elm){
+
+            let rect = elm.getBoundingClientRect();
+            let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+
+            return rect.top + scrollTop; // offset().top;
+
+        },
+        setClassNames : function(elm, classNames){
+
+            if(!classNames){
+
+                return;
+
+            }
+
+            let classNamesArr = classNames.split(' ');
+
+            for(let i=0; i<classNamesArr.length; i++){
+
+                elm.classList.add(classNamesArr[i]);
+
+            }
+
+
+        },
+        setQuery : function () {
+
+            $q = {};
+
+            for(let i=0; i<$e.length; i++){
+
+                let elm = $e[i];
+                let group = elm.group ? elm.group : '$' + $f.getOffsetTop(elm.elm);
+
+                if(elm.elm.hasAttribute('data-show')){
+
+                    continue;
+
+                }
+
+
+                if(typeof $q[group] === "undefined"){
+
+                    $q[group] = [];
+
+                }
+
+                $q[group].push(elm);
+
+            }
+
+        },
+        runQuery : function () {
+
+            let groups = Object.keys($q);
+
+            for(let i=0; i<groups.length; i++){
+
+                let elms = $q[groups[i]];
+
+                if($f.isElementIn(elms[0].elm)){
+
+                    if(elms[0].sort === 'reverse'){
+                        elms.reverse();
+                    }else if(elms[0].sort === 'random'){
+                        $f.randElements(elms);
+                    }
+
+                    $f.sortElements(elms,['order']);
+
+                    let interval = 0;
+
+                    for(let j=0; j<elms.length; j++){
+
+                        (function (j) {
+
+                            elms[j].elm.setAttribute('data-show','true');
+
+                            $f.setClassNames(elms[j].elm,elms[j].addClass);
+
+                            interval = $f.setDurationValue(interval,elms[j-1],elms[j].interval);
+
+                            elms[j].elm.style.animationName = elms[j].cue;
+                            elms[j].elm.style.animationDuration = elms[j].duration + 'ms';
+                            elms[j].elm.style.animationTimingFunction = 'ease';
+                            elms[j].elm.style.animationDelay = interval + 'ms';
+                            elms[j].elm.style.animationDirection = 'normal';
+                            elms[j].elm.style.animationFillMode = 'both';
+
+                        })(j);
+
+                    }
+
+                    delete $q[groups[i]];
+
+                }
+
+
+            }
+
+        },
+        isElementIn : function (elm) {
+
+            return (window.pageYOffset > $f.getOffsetTop(elm)  - window.innerHeight * $op.percentage) || $f.isScrollEnd();
+
+        },
+        isScrollEnd : function () {
+
+            let body = window.document.body;
+            let html = window.document.documentElement;
+            let scrollTop = body.scrollTop || html.scrollTop;
+
+            return scrollTop >= html.scrollHeight - html.clientHeight;
+
+        }
+    };
+
+    return {
+        init : function (options) {
+
+            $op = $f.setOptions($defaultOptions, options);
+            $f.setEvents();
+            $f.searchElements();
+            $f.setQuery();
+
+        },
+        update : function () {
+            $f.searchElements();
+            $f.setQuery();
+            $f.runQuery();
+        }
+    }
+
+})();
